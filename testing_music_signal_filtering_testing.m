84c9c916dc23f9466b7dad9f6b054f022247d2a8
Vishaka Basnayake <vishakabasnayake@gmail.com>
2023-10-26 09:36:33 +0530

    signal analyzer demo

---
+++ testing_music_signal_filtering_testing.m
@@ -0,0 +1,13 @@
+load music
+
+pspectrum(low)
+hold on
+pspectrum(medium)
+hold on
+pspectrum(high)
+hold off
+
+%To hear the different voices
+% sound(low.low,fs), pause(5)
+%sound(medium.medium,fs), pause(5)
+% sound(high.high,fs)
